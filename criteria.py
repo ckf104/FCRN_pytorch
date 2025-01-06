c22642e27f977f8c124af176760f478031e1e4ec
dogeding <913864639@qq.com>
2025-01-06 23:53:42 +0800

    SSIM'

--- criteria.py
+++ criteria.py
@@ -23,12 +23,13 @@ class MaskedMSELoss(nn.Module):
 
 
 class MaskedL1Loss(nn.Module):
-    def __init__(self):
+    def __init__(self, upper_limit=1.0e6):
         super(MaskedL1Loss, self).__init__()
+        self.upper_limit = upper_limit
 
     def forward(self, pred, target):
         assert pred.dim() == target.dim(), "inconsistent dimensions"
-        valid_mask = (target > 0).detach()
+        valid_mask = ((target > 0) & (target < self.upper_limit)).detach()
         diff = target - pred
         diff = diff[valid_mask]
         self.loss = diff.abs().mean()
@@ -36,29 +37,30 @@ class MaskedL1Loss(nn.Module):
 
 
 class berHuLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, upper_limit=1.0e6):
         super(berHuLoss, self).__init__()
+        self.upper_limit = upper_limit
 
     def forward(self, pred, target):
         assert pred.dim() == target.dim(), "inconsistent dimensions"
 
-        huber_c = torch.max(pred - target)
-        huber_c = 0.2 * huber_c
-
+        target = torch.clamp(target, 0.0, self.upper_limit)
         valid_mask = (target > 0).detach()
+
         diff = target - pred
         diff = diff[valid_mask]
         diff = diff.abs()
+        
+        huber_c = torch.max(diff)
+        huber_c = 0.2 * huber_c 
 
         huber_mask = (diff > huber_c).detach()
-
-        diff2 = diff[huber_mask]
-        diff2 = diff2 ** 2
-
-        self.loss = torch.cat((diff, diff2)).mean()
+        diff_1 = diff[~huber_mask]
+        diff_2 = (diff[huber_mask]**2 + huber_c**2) / (2*huber_c)
+                
+        self.loss = torch.cat((diff_1, diff_2)).mean()
 
         return self.loss
-
 
 
 class SSIMLoss(nn.Module):
